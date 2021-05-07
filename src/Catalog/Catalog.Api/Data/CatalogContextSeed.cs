09ce8bc4db7776a4d88b8ab00e56e8cd8fddb2fe
Cuong Nguyen <cuongpianna1996@gmail.com>
2021-05-07 09:23:19 +0700

    .

---
+++ src/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfigurePruducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfigurePruducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "Iphone X",
+                    Description = "Description",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
+    }
+}
---
+++ src/Catalog/Catalog.Api/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
